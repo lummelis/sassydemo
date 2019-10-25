45473dcf8b5e4bc4cdf644bddc53fdfca511c3de
Melissa <lummelis@hawaii.edu>
2019-10-24 14:09:44 -1000

    fixed menubutton to stay at top on scroll

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// mobile nav
 console.log('hello');
 const sitenav = document.querySelector('.site-header .site-nav');
 const menubutton = document.querySelector('.menu-button');
@@ -5,4 +6,12 @@ const menubutton = document.querySelector('.menu-button');
 menubutton.onclick = function () {
     console.log('clicked');
     sitenav.classList.toggle('active');
-}
+};
+
+// Remove active clicked when user click on overlay links
+const sitenavlinks = document.querySelectorAll(".site-header .site-nav a");
+for (var i = 0, len = sitenavlinks.length; i < len; i++) {
+    sitenavlinks[i].onclick = function () {
+        sitenav.classList.remove('active');
+    };
+};
